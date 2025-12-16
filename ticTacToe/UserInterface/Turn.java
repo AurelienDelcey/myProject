320bae969bd491b360c34d59365dcc958f47e39d
AurelienDelcey <aurelien.delcey@gmail.com>
2025-12-16 23:36:17 +0100

    Remove unnecessary comments and clean up Turn.java

--- ticTacToe/UserInterface/Turn.java
+++ ticTacToe/UserInterface/Turn.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ticTacToe.UserInterface;
 
 /**
@@ -12,3 +7,4 @@ package ticTacToe.UserInterface;
 public enum Turn {
     O_TURN,X_TURN;
 }
+
