8813ba24af6ea2dce5c7796caa47c805aafaccc6
AurelienDelcey <aurelien.delcey@gmail.com>
2025-12-16 23:37:20 +0100

    Remove commented license header from Grid.java

--- ticTacToe/DAO/Grid.java
+++ ticTacToe/DAO/Grid.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ticTacToe.DAO;
 
 /**
@@ -50,3 +45,4 @@ public class Grid {
         return true;
     }
 }
+
