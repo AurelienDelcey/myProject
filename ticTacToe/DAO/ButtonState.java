d1f17688daee12fcb4c8a62f29300bae79cb8d8f
AurelienDelcey <aurelien.delcey@gmail.com>
2025-12-16 23:36:56 +0100

    Update ButtonState.java

--- ticTacToe/DAO/ButtonState.java
+++ ticTacToe/DAO/ButtonState.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ticTacToe.DAO;
 
 /**
@@ -12,3 +7,4 @@ package ticTacToe.DAO;
 public enum ButtonState {
     EMPTY,CROSS,CIRCLE;
 }
+
