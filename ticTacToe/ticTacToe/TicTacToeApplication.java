1b3622128d24424649f4bd12ffc31aad980a3123
AurelienDelcey <aurelien.delcey@gmail.com>
2025-12-22 18:39:48 +0100

    Add files via upload

---
+++ ticTacToe/ticTacToe/TicTacToeApplication.java
@@ -0,0 +1,14 @@
+package ticTacToe;
+
+import static javafx.application.Application.launch;
+import ticTacToe.UserInterface.UserInterface;
+
+
+public class TicTacToeApplication {
+
+
+    public static void main(String[] args) {
+        launch(UserInterface.class);
+    }
+
+}
